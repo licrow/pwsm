701877f1927b21e324f2fe2b7ceedf162ca427cb
licrow <39090180+licrow@users.noreply.github.com>
2018-07-26 16:15:17 +0100

    Update pwsm.py

--- src/pwsm.py
+++ src/pwsm.py
@@ -1,1 +1,13 @@
 ### PWSM v0.1
+
+import yaml
+
+# Auxilary Variables
+home_bonus = 0
+
+# Number of players in teamsheet (Subs specified in league.yml)
+num_players = 11
+
+num_subs = 
+
+num_players = num_players + num_subs
